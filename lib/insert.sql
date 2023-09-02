6d90b426f477b085288757d6f5a7f06e5baab537
Abigail Amani Wanjiku <abigail.wanjiku@student.moringaschool.com>
2023-09-02 16:13:42 +0300

    create and insert tests

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Mr. Chocolate', 24, 'M', 'blue', 'playful', 1)
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Rowdy', 12, 'M', 'brown', 'calm', 1)
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Tabitha', 16, 'F', 'purple', 'artistic', 1)
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Sergeant Brown', 46, 'M', 'grey', 'respectful', 0)
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Melissa', 30, 'F', 'green', 'girly', 1)
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Grinch', 57, 'M', 'brown', 'angry', 1)
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Wendy', 6, 'F', 'white', 'innovative', 1)
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('NULL', 15, 'M', 'yellow', 'loud', 0)
